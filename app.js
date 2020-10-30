c86c50842fe04aa5bac38a8f55456a41c399ae30
Bryan <bryanjguilas@gmail.com>
2020-10-29 21:10:34 -0700

    added time Remaining

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //create a class
-// will have a start method, pause method, onDurationChange, tick method
+//will have a start method, pause method, onDurationChange, tick method
 
 class Timer {
   constructor(durationInput, startButton, pauseButton) {
@@ -16,19 +16,20 @@ class Timer {
   start = () => {
     // This will start the timer immediately instead of waiting a sec before start
     this.tick();
-    this.timer = setInterval(this.tick, 1000);
+    this.interval = setInterval(this.tick, 1000);
   };
 
   pause = () => {
-    clearInterval(this.timer);
+    clearInterval(this.interval);
   };
 
   tick = () => {
-    console.log('tick');
+    const timeRemaining = parseFloat(this.durationInput.value);
+    this.durationInput.value = timeRemaining - 1;
   };
 }
 
-const durationInput = document.querySelector('#input');
+const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
