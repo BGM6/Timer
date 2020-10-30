9ec5aad33840b87fe9330b7a6b75bf95b242a461
Bryan <bryanjguilas@gmail.com>
2020-10-29 20:49:19 -0700

    start, pause method created

--- app.js
+++ app.js
@@ -3,16 +3,29 @@
 
 class Timer {
   constructor(durationInput, startButton, pauseButton) {
+    // THIS - start lives here
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
   }
 
-  start() {
-    console.log('Time to start the timer');
-  }
+  // Using an arrow function here allows 'THIS' to live in the class
+  start = () => {
+    // This will start the timer immediately instead of waiting a sec before start
+    this.tick();
+    this.timer = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.timer);
+  };
+
+  tick = () => {
+    console.log('tick');
+  };
 }
 
 const durationInput = document.querySelector('#input');
