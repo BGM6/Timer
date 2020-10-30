472f9896fcd2a2ab74dcdb17e45a0644dfc0150d
Bryan <bryanjguilas@gmail.com>
2020-10-29 22:12:11 -0700

    timer logic complete

--- app.js
+++ app.js
@@ -1,49 +1,19 @@
 //Create a class
 //Will have a start method, pause method, onDurationChange, tick method
-
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    // THIS - start lives here
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-
-  // Using an arrow function here allows 'THIS' to live in the class
-  start = () => {
-    // This will start the timer immediately instead of waiting a sec before start
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    // const timeRemaining = this.timeRemaining;
-    // this.timeRemaining = timeRemaining - 1;
-    if (this.timeRemaining <= 0) {
-      this.pause();
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-    }
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('Timer has started!');
+  },
+
+  onTick() {
+    console.log('Timer just ticked down');
+  },
+
+  onComplete() {
+    console.log('Timer is completed');
+  },
+});
---
+++ timer.js
@@ -0,0 +1,55 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    // THIS - start lives here
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  // Using an arrow function here allows 'THIS' to live in the class
+  start = () => {
+    // This will start the timer immediately instead of waiting a sec before start
+    if (this.onStart) {
+      this.onStart();
+    }
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    // const timeRemaining = this.timeRemaining;
+    // this.timeRemaining = timeRemaining - 1;
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
